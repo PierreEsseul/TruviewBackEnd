be85ffca6341950479d5380608f3f270603bb102
Pierre <p.esseul@gmail.com>
2023-05-03 14:51:13 +0200

    add loader

--- routes/route.js
+++ routes/route.js
@@ -5,15 +5,11 @@ const router = express.Router();
 router.post('/', async (req, res, next) => {
   try {
     const { language, network, about } = req.body;
-
-    console.log("----------BEFORE")
-    
     const model = 'davinci';
-    // const prompt = `${about} ${network} ${language}`;
-    const prompt = about;
+    const prompt = `I would like you to write about :${about}, for this network :${network} in this language : ${language}. Please make sure to include relevant hashtags and emojis to attract user attention.`;
     const parameters = {
-      maxTokens: 150,
-      temperature: 0.7,
+      maxTokens: 200,
+      temperature: 0.5,
     };
     
     const response = await fetch('https://api.openai.com/v1/engines/' + model + '/completions', {
@@ -31,10 +27,8 @@ router.post('/', async (req, res, next) => {
     
  
     const data = await response.json();
-    console.log("----------AFTER", data)
     
   const generatedText = data.choices[0].text;
-    console.log(generatedText);
     res.status(200).json({success: true, data: generatedText})
 } catch (e) {
   console.log(e);
