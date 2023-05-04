8f150c0063d4cf61d703043ab862e82d308501ec
Pierre <p.esseul@gmail.com>
2023-05-04 12:50:04 +0200

    modif prompt

--- routes/route.js
+++ routes/route.js
@@ -6,10 +6,11 @@ router.post('/', async (req, res, next) => {
   try {
     const { language, network, about } = req.body;
     const model = 'davinci';
-    const prompt = `I would like you to write about :${about}, for this network :${network} in this language : ${language}. Please make sure to include relevant hashtags and emojis to attract user attention.`;
+    const prompt =  `${language} ${about} ${network}`;
+    console.log('Initial prompt: ', prompt);
     const parameters = {
       maxTokens: 200,
-      temperature: 0.5,
+      temperature: 0.4,
     };
     
     const response = await fetch('https://api.openai.com/v1/engines/' + model + '/completions', {
@@ -29,7 +30,8 @@ router.post('/', async (req, res, next) => {
     const data = await response.json();
     
   const generatedText = data.choices[0].text;
-    res.status(200).json({success: true, data: generatedText})
+  console.log('Generated text: ', generatedText);
+  res.status(200).json({success: true, data: generatedText})
 } catch (e) {
   console.log(e);
 }
